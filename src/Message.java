9ab1b6792ee29c7403c511d47aeb4fe68524cc1d
Alexandra Bystrova <saobyjob90@gmail.com>
2022-02-05 12:49:15 +0300

    First commit

---
+++ src/Message.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+public class Message {
+    public static void main(String[] args) {
+        String name = "Иванова Мария";
+        int age = 30;
+        String experience = "Нет опыта";
+        String text = "Тестировщица: "+ name;
+        text = text +"\n"+"Возраст: "+ age + " лет";
+        text = text + '\n'+ "Опыт работы: " + experience;
+        JOptionPane.showMessageDialog(null,text);
+
+    }
+}
